886e5c762cde3a7312ef33bc23c8f88ecf57dcc1
Matthew Reilly <mreill11@nd.edu>
2018-01-23 13:31:12 -0500

    File I/O

--- Assignment1/crawler.py
+++ Assignment1/crawler.py
@@ -13,11 +13,15 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 test = api.lookup_users(user_ids=['34373370, 26257166, 12579252'])
 
+f = open('user_info.txt', 'w')
+
 for user in test:
-    print "Screen Name: " + user.screen_name
-    print "User Name: " + user.name
-    print "User Description: " + user.description
-    print "Number of Followers: %d" % user.followers_count
-    print "Number of Statuses: %d" % user.statuses_count
-    print "User's URL: " + user.url
-    print "\n"
+    f.write("Screen Name: " + user.screen_name + "\n")
+    f.write("User Name: " + user.name + "\n")
+    f.write("User Description: " + user.description + "\n")
+    f.write("Number of Followers: %d\n" % user.followers_count)
+    f.write("Number of Statuses: %d\n" % user.statuses_count)
+    f.write("User's URL: " + user.url + "\n")
+    f.write("\n")
+
+f.close()
