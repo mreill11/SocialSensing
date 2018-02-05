ff03ebfa864edc6ee18738249a86faf054e505b6
Matthew Reilly <mreill11@nd.edu>
2018-02-05 11:09:43 -0500

    Finished

---
+++ Assignment1/README.md
@@ -0,0 +1,6 @@
+To run my crawler:
+```python crawler.py```
+
+I used Python 2.7.
+
+I used the streaming API for tweets by location, so it runs until it gets 50 tweets inside the location bounds.
--- Assignment1/crawler.py
+++ Assignment1/crawler.py
@@ -13,16 +13,58 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 test = api.lookup_users(user_ids=['34373370, 26257166, 12579252'])
 
-f = open('user_info.txt', 'w')
+# f = open('user_info.txt', 'w')
+#
+# for user in test:
+#     f.write("Screen Name: " + user.screen_name + "\n")
+#     f.write("User Name: " + user.name + "\n")
+#     f.write("User Description: " + user.description + "\n")
+#     f.write("Number of Followers: %d\n" % user.followers_count)
+#     f.write("Number of Friends: %d\n" % user.friends_count)
+#     f.write("Number of Statuses: %d\n" % user.statuses_count)
+#     f.write("User's URL: " + user.url + "\n")
+#     f.write("\n")
+#
+# f.close()
+#
+# f = open('user_network.txt', 'w')
+# for user in test:
+#     user_followers = tweepy.Cursor(api.followers, screen_name=user.screen_name).items(20)
+#     f.write("\nThe Followers List:\n\n")
+#     for u in user_followers:
+#         f.write(u.screen_name + "\n")
+#     user_friends = tweepy.Cursor(api.friends, screen_name=user.screen_name).items(20)
+#     f.write("\nThe Friends List:\n\n")
+#     for u in user_friends:
+#         f.write(u.screen_name + "\n")
+#
+# f.close()
 
-for user in test:
-    f.write("Screen Name: " + user.screen_name + "\n")
-    f.write("User Name: " + user.name + "\n")
-    f.write("User Description: " + user.description + "\n")
-    f.write("Number of Followers: %d\n" % user.followers_count)
-    f.write("Number of Friends: %d\n" % user.friends_count)
-    f.write("Number of Statuses: %d\n" % user.statuses_count)
-    f.write("User's URL: " + user.url + "\n")
-    f.write("\n")
-
+f = open('keyword_tweets.txt', 'w')
+query = '"Indiana" OR "weather"'
+maxTweets = 50
+for tweet in tweepy.Cursor(api.search,q=query).items(maxTweets):
+    f.write(tweet.text.encode('utf-8') + "\n")
 f.close()
+
+f = open('location_tweets.txt', 'w')
+numTweets = 0
+class CustomStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        global numTweets
+        if numTweets <= 50:
+            f.write(status.text.encode('utf-8') + "\n")
+            numTweets += 1
+        else:
+            return True
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+
+sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+sapi.filter(locations=[-86.33,41.63,-86.20,41.74])
